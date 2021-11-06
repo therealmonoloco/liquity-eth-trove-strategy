fb0913fc2b2267e2dd144d1d6072b518b0858c00
therealmonoloco <therealmonoloco@gmail.com>
2021-11-06 16:11:48 -0400

    feat: add basic yearn and liquity interfaces

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -1,15 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0
-// Feel free to change the license, but this is what we use
-
-// Feel free to change this version of Solidity. We support >=0.6.0 <0.7.0;
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
 // These are the core Yearn libraries
-import {
-    BaseStrategy,
-    StrategyParams
-} from "@yearnvaults/contracts/BaseStrategy.sol";
+import {BaseStrategy} from "@yearnvaults/contracts/BaseStrategy.sol";
 import {
     SafeERC20,
     SafeMath,
@@ -17,26 +11,122 @@ import {
     Address
 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
-// Import interfaces for many popular DeFi projects, or add your own!
-//import "../interfaces/<protocol>/<Interface>.sol";
+import "../interfaces/liquity/IBorrowerOperations.sol";
+import "../interfaces/liquity/IPriceFeed.sol";
+import "../interfaces/yearn/IBaseFee.sol";
+import "../interfaces/yearn/IVault.sol";
 
 contract Strategy is BaseStrategy {
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
 
-    constructor(address _vault) public BaseStrategy(_vault) {
-        // You can set these parameters on deployment to whatever you want
-        // maxReportDelay = 6300;
-        // profitFactor = 100;
-        // debtThreshold = 0;
+    // LUSD token
+    IERC20 internal constant investmentToken =
+        IERC20(0x5f98805A4E8be255a32880FDeC7F6728C6568bA0);
+
+    // Chainlink ETH:USD with Tellor ETH:USD as fallback
+    IPriceFeed internal constant priceFeed =
+        IPriceFeed(0x4c517D4e2C851CA76d7eC94B805269Df0f2201De);
+
+    // Provider to read current block's base fee
+    IBaseFee internal constant baseFeeProvider =
+        IBaseFee(0xf8d0Ec04e94296773cE20eFbeeA82e76220cD549);
+
+    // 100%
+    uint256 internal constant MAX_BPS = 1e18;
+
+    // Maximum loss on withdrawal from yVault
+    uint256 internal constant MAX_LOSS_BPS = 10000;
+
+    // LUSD yVault
+    IVault public yVault;
+
+    // Our desired collaterization ratio
+    uint256 public collateralizationRatio;
+
+    // Allow the collateralization ratio to drift a bit in order to avoid cycles
+    uint256 public rebalanceTolerance;
+
+    // Max acceptable base fee to take more debt or harvest
+    uint256 public maxAcceptableBaseFee;
+
+    // Maximum acceptable loss on withdrawal. Default to 0.01%.
+    uint256 public maxLoss;
+
+    // Minimum collateralization ratio to enforce
+    uint256 internal minCollatRatio;
+
+    constructor(address _vault, address _yVault) public BaseStrategy(_vault) {
+        yVault = IVault(_yVault);
+
+        // Set health check to health.ychad.eth
+        healthCheck = 0xDDCea799fF1699e98EDF118e0629A974Df7DF012;
+
+        // Current ratio can drift (collateralizationRatio - rebalanceTolerance, collateralizationRatio + rebalanceTolerance)
+        // Allow additional 30% in any direction (170, 230) by default
+        rebalanceTolerance = (30 * MAX_BPS) / 100;
+
+        // Use 200% as target
+        collateralizationRatio = (200 * MAX_BPS) / 100;
+
+        // Never allow rebalancing band to go below 150%
+        // Troves below 150% become liquidatable if system goes into Recovery Mode
+        minCollatRatio = (150 * MAX_BPS) / 100;
+
+        // Define maximum acceptable loss on withdrawal to be 0.01%.
+        maxLoss = 1;
+
+        // Set max acceptable base fee to take on more debt to 60 gwei
+        maxAcceptableBaseFee = 60 gwei;
+    }
+
+    // ----------------- SETTERS & MIGRATION -----------------
+
+    // Maximum acceptable base fee of current block to take on more debt
+    function setMaxAcceptableBaseFee(uint256 _maxAcceptableBaseFee)
+        external
+        onlyEmergencyAuthorized
+    {
+        maxAcceptableBaseFee = _maxAcceptableBaseFee;
+    }
+
+    // Target collateralization ratio to maintain within bounds
+    function setCollateralizationRatio(uint256 _collateralizationRatio)
+        external
+        onlyEmergencyAuthorized
+    {
+        require(
+            _collateralizationRatio.sub(rebalanceTolerance) > minCollatRatio
+        ); // dev: desired collateralization ratio is too low
+        collateralizationRatio = _collateralizationRatio;
+    }
+
+    // Rebalancing bands (collat ratio - tolerance, collat_ratio + tolerance)
+    function setRebalanceTolerance(uint256 _rebalanceTolerance)
+        external
+        onlyEmergencyAuthorized
+    {
+        require(
+            collateralizationRatio.sub(_rebalanceTolerance) > minCollatRatio
+        ); // dev: desired rebalance tolerance makes allowed ratio too low
+        rebalanceTolerance = _rebalanceTolerance;
+    }
+
+    // Max slippage to accept when withdrawing from yVault
+    function setMaxLoss(uint256 _maxLoss) external onlyVaultManagers {
+        require(_maxLoss <= MAX_LOSS_BPS); // dev: invalid value for max loss
+        maxLoss = _maxLoss;
     }
 
     // ******** OVERRIDE THESE METHODS FROM BASE CONTRACT ************
 
     function name() external view override returns (string memory) {
-        // Add your own name here, suggestion e.g. "StrategyCreamYFI"
-        return "Strategy<ProtocolName><TokenType>";
+        return "StrategyLiquityTroveETH";
+    }
+
+    function delegatedAssets() external view override returns (uint256) {
+        return _convertInvestmentTokenToWant(_valueOfInvestment());
     }
 
     function estimatedTotalAssets() public view override returns (uint256) {
@@ -93,19 +183,6 @@ contract Strategy is BaseStrategy {
         // NOTE: `migrate` will automatically forward all `want` in this strategy to the new one
     }
 
-    // Override this to add all tokens/tokenized positions this contract manages
-    // on a *persistent* basis (e.g. not just for swapping back to want ephemerally)
-    // NOTE: Do *not* include `want`, already included in `sweep` below
-    //
-    // Example:
-    //
-    //    function protectedTokens() internal override view returns (address[] memory) {
-    //      address[] memory protected = new address[](3);
-    //      protected[0] = tokenA;
-    //      protected[1] = tokenB;
-    //      protected[2] = tokenC;
-    //      return protected;
-    //    }
     function protectedTokens()
         internal
         view
@@ -113,20 +190,6 @@ contract Strategy is BaseStrategy {
         returns (address[] memory)
     {}
 
-    /**
-     * @notice
-     *  Provide an accurate conversion from `_amtInWei` (denominated in wei)
-     *  to `want` (using the native decimal characteristics of `want`).
-     * @dev
-     *  Care must be taken when working with decimals to assure that the conversion
-     *  is compatible. As an example:
-     *
-     *      given 1e17 wei (0.1 ETH) as input, and want is USDC (6 decimals),
-     *      with USDC/ETH = 1800, this should give back 1800000000 (180 USDC)
-     *
-     * @param _amtInWei The amount (in wei/1e-18 ETH) to convert to `want`
-     * @return The amount in `want` of `_amtInEth` converted to `want`
-     **/
     function ethToWant(uint256 _amtInWei)
         public
         view
@@ -134,7 +197,33 @@ contract Strategy is BaseStrategy {
         override
         returns (uint256)
     {
-        // TODO create an accurate price oracle
-        return _amtInWei;
+        return _amtInWei.mul(priceFeed.lastGoodPrice()).div(1e18);
+    }
+
+    // ----------------- INTERNAL CALCS -----------------
+
+    function _valueOfInvestment() internal view returns (uint256) {
+        return
+            yVault.balanceOf(address(this)).mul(yVault.pricePerShare()).div(
+                10**yVault.decimals()
+            );
+    }
+
+    function _investmentTokenToYShares(uint256 amount)
+        internal
+        view
+        returns (uint256)
+    {
+        return amount.mul(10**yVault.decimals()).div(yVault.pricePerShare());
+    }
+
+    // ----------------- TOKEN CONVERSIONS -----------------
+
+    function _convertInvestmentTokenToWant(uint256 amount)
+        internal
+        view
+        returns (uint256)
+    {
+        return amount.mul(1e18).div(priceFeed.lastGoodPrice());
     }
 }
---
+++ interfaces/liquity/IPriceFeed.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+interface IPriceFeed {
+    // --- Events ---
+    event LastGoodPriceUpdated(uint256 _lastGoodPrice);
+
+    // --- Function ---
+    function fetchPrice() external returns (uint256);
+
+    function lastGoodPrice() external view returns (uint256);
+}
